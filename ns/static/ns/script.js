f53bac94149715b10a5b7c27aa57b86db70aacf1
snchzzero <aaevstafev@gmail.com>
2022-10-03 00:36:10 +0300

    slick(3)

--- ns/static/ns/script.js
+++ ns/static/ns/script.js
@@ -1,18 +1,47 @@
 $('.slider_for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
+  arrows: false, // arrows - показ стрелки
   fade: true,
   asNavFor: '.slider'
 });
 
+//$(document).ready(function(){
+//  $('.slider').slick({
+//        arrows: true,
+//        slidesToShow: 5,
+//        slidesToScroll: 1,
+//        asNavFor: '.slider_for',
+//        dots: true,
+//        centerMode: true,
+//        focusOnSelect: true
+//  });
+//});
 $(document).ready(function(){
   $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider_for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true
+  centerMode: true,
+  centerPadding: '60px',  //'60px'
+  slidesToShow: 5,
+  asNavFor: '.slider_for',
+  responsive: [
+    {
+      breakpoint: 768, //768
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '40px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 480, //480
+      settings: {
+        arrows: false,
+        centerMode: false,
+        centerPadding: '40px', //'40px'
+        slidesToShow: 1
+      }
+    }
+  ]
   });
 });

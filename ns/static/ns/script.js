243acc36e90829e2ad5d1c8e59f696631a6849ce
snchzzero <aaevstafev@gmail.com>
2022-10-02 21:13:43 +0300

    slick(2)

--- ns/static/ns/script.js
+++ ns/static/ns/script.js
@@ -1,5 +1,18 @@
+$('.slider_for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider'
+});
+
 $(document).ready(function(){
   $('.slider').slick({
-
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider_for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
   });
 });

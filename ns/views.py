e6458639fa45471cbeea2ba20954d34dde921843
snchzzero <aaevstafev@gmail.com>
2022-10-10 12:26:06 +0300

    filer-slider

--- ns/views.py
+++ ns/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from ns.models import My_image
 
 # Create your views here.
 
 def promo(request):
+    images_slider = My_image.objects.all()
+    print(images_slider)
+    for i in images_slider:
+        print(i.cover)
+        print(i.title)
+        print(i.cover.url)
+        print()
     if request.method == 'GET':
-        return render(request, 'ns/promo.html')
+        return render(request, 'ns/promo.html', {'imgs': images_slider})
 
 def test(request):
     if request.method == 'GET':

626b7068d8eb175daba3fbbdbfdc91ccc8318db8
snchzzero <aaevstafev@gmail.com>
2022-10-12 13:36:27 +0300

    django-sortable2

--- ns/views.py
+++ ns/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
-from ns.models import My_image
+from ns.models import My_image, SortableMy_image
 
 # Create your views here.
 
 def promo(request):
     images_slider = My_image.objects.all()
-    print(images_slider)
-    for i in images_slider:
-        print(i.cover)
-        print(i.title)
-        print(i.cover.url)
-        print()
-    if request.method == 'GET':
-        return render(request, 'ns/promo.html', {'imgs': images_slider})
+    all_images = list()
+    for image in images_slider:
+        image_sl = SortableMy_image.objects.all().filter(title=image)
+        images = [img.image.url for img in image_sl]
+        image_id = '{}_{}'.format(image.title, image.id)
+        nav_image_id = 'nav_{}_{}'.format(image.title, image.id)
+        if image.cover:
+            cover = 'url("{}")'.format(image.cover.url)
+        else:
+            cover = '#507D2A'
+        data_image = {
+            'title': image.title,
+            'cover': cover,
+            'image_id': image_id,
+            'nav_image_id': nav_image_id,
+            'images': images
 
-def test(request):
-    if request.method == 'GET':
-        return render(request, 'ns/test.html')
+        }
+        all_images.append(data_image)
+
+    return render(request, 'ns/promo.html', {'imgs': all_images})
+
+
+
+
+
